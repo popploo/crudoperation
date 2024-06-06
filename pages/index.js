430a18e29594230321303bcbdc807bd9a26a042f
Hamza Ashraf <fa22adsd0004@maju.edu.pk>
2024-06-06 19:23:54 +0500

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import Navbar from "../Components/Navbar";
+import { Layout } from "../Components/Layout/Layout";
+
+
+export default function Home(props) {
+  return (
+    <>
+       
+    </>
+  );
+}
